cf07588abdeb7a9d0c18ffd1b04ff7764b1f5d41
Hedy Wang <hedywings@gmail.com>
2016-06-23 11:15:29 +0800

    add sivann modules to whitelist

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function(central) {
+    central.blocker(true, 'white');
+    central.allow('0xd05fb820a6c4');
+    central.allow('0x5c313e2bfb08');
+    central.allow('0xd05fb820e4eb');
+    central.allow('0xd05fb820c22a');
+};
